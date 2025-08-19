7b62342a4b499f3216ed72c5f0b48b1aeb686aea
axiomcura <erikishere3@gmail.com>
2025-08-19 13:00:04 -0600

    nb converted notebooks

---
+++ notebooks/0.download-data/nbconverted/3.subset-jump-controls.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# # 3. Subsetting CPJUMP1 controls
+
+# In[6]:
+
+
+import pathlib
+
+import pandas as pd
+
+# In[3]:
+
+
+# setting data path
+data_path = pathlib.Path("./data/CPJUMP1-experimental-metadata.csv")
+
+
+# In[7]:
+
+
+pd.read_csv(data_path).head(100)
+
+
+# In[ ]:
