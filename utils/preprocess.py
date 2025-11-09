73faba0a4acdb78f42624ed6ceee5a1af9acef9d
Erik Serrano <erikishere3@gmail.com>
2025-11-09 10:14:28 -0700

    updated parameter name

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -16,7 +16,7 @@ def apply_pca(
     var_explained: float | int = 0.95,
     svd_solver="full",
     standardize=False,
-    random_state=0,
+    seed=0,
     **kwargs,
 ) -> pl.DataFrame:
     """Apply PCA to the morphological features of the profiles DataFrame.
@@ -89,7 +89,7 @@ def apply_pca(
     # apply PCA
     pca = PCA(
         n_components=var_explained,
-        random_state=random_state,
+        random_state=seed,
         svd_solver=svd_solver,
         **kwargs,
     )
